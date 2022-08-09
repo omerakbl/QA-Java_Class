e8c2fe398a374bb17ec5bf3d003f32d8bb9e8dc7
Omer <omer.akbl@gmail.com>
2022-08-09 01:25:13 -0400

    Wrapper Classes

---
+++ AllClass/src/Test_Night_022/c1_WrapperClasses.java
@@ -0,0 +1,45 @@
+package Test_Night_022;
+
+public class c1_WrapperClasses {
+
+    /*
+    Java Wrapper Classes
+    Java Wrapper Classes provide a way to use primitive data types (int,boolean, etc..) as object
+
+    The table below shows the primitive type and equivalent wrapper classes:
+
+    Primitive Data Type                 Wrapper Classes
+    byte                                Byte
+    short                               Short
+    int                                 Integer
+    long                                Long
+    float                               Float
+    double                              Double
+    boolean                             Boolean
+    char                                Character
+
+    Sometimes you must use wrapper classes, for example when working with Collection object, such as ArrayList,
+    where primitive types cannot be used (the list can only store objects):
+
+    Example
+    ArrayList<int> myNumbers = new ArrayList<int>(); // Invalid
+    ArrayList(Integer) myNumbers = new ArrayList<Integer>(); // Valid
+
+    Data Structure:
+        1. Array
+        2. Collection ==> only accepts objects
+        3. Map ==> only accepts objects
+
+
+    Primitives      : byte, short, int, long, float, double, char, boolean
+
+    Wrapper Classes : Byte, Short, Integer, Long, Float, Double, Char, Boolean
+
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
