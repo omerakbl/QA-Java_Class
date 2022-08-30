79fb96cbc78aeebaf68c1bb7bd4ed9dce9cf1c4c
Omer <omer.akbl@gmail.com>
2022-08-29 21:46:42 -0400

    Static Example

---
+++ AllClass/src/Test_Night_030/c9_staticExample.java
@@ -0,0 +1,19 @@
+package Test_Night_030;
+
+public class c9_staticExample {
+    static {
+        System.out.println("hello world");
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    {
+        System.out.println("dasbdaksb");
+    }
+
+
+}
+
